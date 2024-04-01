c51751dae7c3e48a2d05c68c3fb252bac70e3b4d
Lythmass <lythmass17@gmail.com>
2024-04-01 12:45:08 +0400

    feat: add bug model

---
+++ app/models/Bug.py
@@ -0,0 +1,43 @@
+from main import db
+from app.models.Severity import Severity
+from app.models.Status import Status
+from app.models.Project import Project
+from app.models.User import User
+import datetime
+
+class Bug(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  description = db.Column(db.Text, nullable=False)
+  solution = db.Column(db.Text, nullable=True)
+  severity_id = db.Column(db.Integer, db.ForeignKey('severity.id'), nullable=False)
+  severity = db.relationship(Severity, backref=db.backref('bug'))
+  status_id = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
+  status = db.relationship(Status, backref=db.backref('bug'))
+  project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+  reporter_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  reporter = db.relationship(User, foreign_keys=[reporter_id], backref=db.backref('bug'))
+  resolver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+  resolver = db.relationship(User, foreign_keys=[resolver_id], backref=db.backref('bug'))
+  created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
+  updated_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
+  
+  def to_dict(self):
+    return {
+      "name": self.name,
+      "description": self.description,
+      "solution": self.solution,
+      "severity": self.severity.severity,
+      "status": self.status.status,
+      "project_id": self.project_id,
+      "reporter": {
+        "name": self.reporter.name, 
+        "last_name": self.reporter.last_name,
+        "username": self.reporter.username,
+      },
+      "resolver": {
+        "name": self.resolver.name, 
+        "last_name": self.resolver.last_name,
+        "username": self.resolver.username,
+      },
+    }
